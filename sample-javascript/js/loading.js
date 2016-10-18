b519c41ee17cb964b99c2cd9aea71483de92f9de
Your Name <you@example.com>
2016-10-18 16:20:41 +0900

    Bootstrap Javascript ~ Collapse Sample

---
+++ sample-javascript/js/loading.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('#loadingBtn').on('click', function() {
+    var $btn = $(this).button('loading');
+    // business logic...
+
+    setTimeout(function() {
+      $btn.button('reset');
+    }, 2000);
+  });
+
+  $('checkboxes label.btn.active').removeClass('btn-primary').addClass('btn-success');
+  // 체크박스 버튼 그룹 클릭 이벤트 코드
+  $('#checkboxes label.btn').on('click', function() {
+    if($(this).hasClass('active')) {
+      $(this).removeClass('btn-success').addClass('btn-primary');
+    } else {
+      $(this).removeClass('btn-primary').addClass('btn-success');
+    }
+  });
+
+  $('radioboxes label.btn.active').removeClass('btn-primary').addClass('btn-success');
+  // 라디오박스 버튼 그룹 클릭 이벤트 코드
+  $('#radioboxes label.btn').on('click', function() {
+    // 이미 기존에 활성화 된 버튼의 클래스를 원래대로 만들어준다.
+    $('#radioboxes label.btn.btn-success').removeClass('btn-success').addClass('btn-primary');
+    // 선택한 버튼의 색 클래스를 활성화 색 클래스로 바꿔준다.
+    $(this).removeClass('btn-primary').addClass('btn-success');
+  });
+});
--- sample-javascript/js/tabnav.js
+++ sample-javascript/js/tabnav.js
@@ -3,5 +3,19 @@
 // });
 
 $(function() {
-  $('#myTab a:list').tab('show');
+  $('#myTab a:first').tab('show');
+  $('#yourTab').on('shown.bs.tab', function (event) {
+    // 이전에 선택되었던 탭메뉴의 아이콘
+    $(event.relatedTarget).find('i').remove();
+    // 현재 선택한 탭메뉴에 아이콘을 추가하는 코드
+    $(event.target).prepend('<i class="fa fa-arrow-right"></i>');
+  });
+
+  $('#myTab').on('shown.bs.tab', function (event) {
+    console.log(1);
+    // 이전에 선택되었던 탭메뉴의 아이콘
+    $(event.relatedTarget).find('i').css('display', 'none');
+    // 현재 선택한 탭메뉴에 아이콘을 추가하는 코드
+    $(event.target).find('i').css('display', 'inline')
+  });
 });
